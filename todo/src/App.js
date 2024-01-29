b783da4beab810f3f86b35c61ab4de097a9574d0
chothaniparth <138977463+chothaniparth@users.noreply.github.com>
2024-01-29 11:27:58 +0530

    init & made react boiler plate

---
+++ todo/src/App.js
@@ -0,0 +1,29 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <table>
+        <tr>
+          <th>Name</th>
+          <th>Age</th>
+        </tr>
+        <tr>
+          <td>parth</td>
+          <td>19</td>
+        </tr>
+        <tr>
+          <td>krishna</td>
+          <td>22</td>
+        </tr>
+        <tr>
+          <td>shyam</td>
+          <td>24</td>
+        </tr>
+      </table>
+    </div>
+  );
+}
+
+export default App;
---
+++ todo/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+// const Check = <p>, I am Learning react.js</p>;
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <App />
+);
+
+reportWebVitals();
